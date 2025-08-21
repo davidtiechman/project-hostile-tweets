7181370d7d9999226bef910e12b5b06c0d50fadb
Knobloch Moshe <moskno1@gmail.com>
2025-08-21 15:10:29 +0300

    finish the tree function for processor

--- app/processor.py
+++ app/processor.py
@@ -1,6 +1,6 @@
+from nltk.sentiment import vader
 from app.retcher import load_data, convert_by_df
 from collections import Counter
-import pandas as pd
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
@@ -16,11 +16,27 @@ class Analyzer():
     def emotion_text(self):
         nltk.download('vader_lexicon')  # Compute sentiment labels
         # tweet = self.df['Text'].loc[0]
-        tweet = 'i wolking to israel '
-        score = SentimentIntensityAnalyzer().polarity_scores(tweet)
-        pass
+        self.df['score'] = self.df['Text'].apply(lambda text: vader.SentimentIntensityAnalyzer().polarity_scores(str(text))['compound'])
+        self.df['type_text'] = self.df['score'].apply(lambda s: 'positive' if s >= 0.5 else ('negative' if s <= -0.5 else 'neutral'))
+        self.df.drop(columns=['score'], inplace=True)
+        return self.df
     def find_weapon_name(self):
-        pass
+        arr_weapon = []
+        with open('../data/weapon_list.txt', 'r') as file:
+            for name_weapon in file.readlines():
+                nwe_name = name_weapon.strip('\n')
+                arr_weapon.append(nwe_name)
+            for name in arr_weapon:
+                if name not in self.df.index:
+                    self.df['weapons_detected'] = ''
+                elif name in self.df.index:
+                    self.df['weapons_detected'] += name + ' '
+        print(arr_weapon)
+        print(self.df['weapons_detected'])
+        print(self.df.columns)
+         # self.df['Text']
+        # pass
 analyzer = Analyzer()
-print(analyzer.rarest_word())
-analyzer.emotion_text()
+# analyzer.rarest_word()
+# analyzer.emotion_text()
+analyzer.find_weapon_name()
--- app/retcher.py
+++ app/retcher.py
@@ -1,2 +1,13 @@
+import pymongo
+import pandas as pd
+def load_data():
+    myclient = pymongo.MongoClient(host="mongodb+srv://IRGC:iraniraniran@iranmaldb.gurutam.mongodb.net/")
+    mydb = myclient["IranMalDB"]
+    mycol = mydb["tweets"]
+    mongo = mycol.find()
+    return mongo
 
-df =
+def convert_by_df(mongo):
+    mongo = list(mongo)
+    df = pd.DataFrame(mongo)
+    return df
